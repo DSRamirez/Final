c22e024907233ee0862acc535316bba17f1d0c79
Ezequiel Trejo Mercado <ezetrejom75@gmail.com>
2021-03-23 17:01:52 -0300

    .

---
+++ Blazor/Blazor/Server/Migrations/20210323020310_Turo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blazor.Server.Migrations
+{
+    public partial class Turo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ControlTurnos",
+                columns: table => new
+                {
+                    Id_Turno = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nº_Turno = table.Column<int>(type: "int", nullable: false),
+                    Nick = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Peluquero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Servicio = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Dia_Y_Hora = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ControlTurnos", x => x.Id_Turno);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ControlTurnos");
+        }
+    }
+}
