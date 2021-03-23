d77119e7ed70331c029871b8c65c76d015ae7f94
Ezequiel Trejo Mercado <ezetrejom75@gmail.com>
2021-03-22 21:51:40 -0300

    Actualizacion

---
+++ Blazor/Blazor/Server/Models/Turnos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.Server.Models
+{
+    public class Turnos
+    {
+        [Key]
+        public int Id_Turno { get; set; }
+        [Required]
+        public int Nº_Turno { get; set; }
+        [Required]
+        public string Nick { get; set; }
+        [Required]
+        public string Peluquero { get; set; }
+        [Required]
+        public string Servicio { get; set; }
+        [Required]
+        public DateTime Dia_Y_Hora { get; set; }
+    }
+}
