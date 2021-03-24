8420220af9dda9bfe3d6604e62b2543e25d62006
Ezequiel Trejo Mercado <ezetrejom75@gmail.com>
2021-03-24 19:04:15 -0300

    .

--- Blazor/Blazor/Server/Controllers/TurnosController.cs
+++ Blazor/Blazor/Server/Controllers/TurnosController.cs
@@ -2,11 +2,80 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blazor.Server.Data;
+using Blazor.Server.Models;
+using Microsoft.EntityFrameworkCore.Internal;
 
-namespace Blazor.Server.Controllers
+namespace ApiStock.Controllers
 {
-    public class TurnosController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurnosController : ControllerBase
     {
+        private readonly ApiContext _context;
 
+        public TurnosController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Turnos>> Get()
+        {
+            return _context.ControlTurnos.Include(p => p).ToList();
+        }
+        // GET: api/Pais/5
+        [HttpGet("{id}", Name = "ObtenerTurnoPorId")]
+        public ActionResult<Turnos> Get(int id)
+        {
+            var turno = _context.ControlTurnos.FirstOrDefault(p => p.Id_Turno == id);
+            if (turno == null)
+            {
+                return NotFound();
+            }
+            return turno;
+        }
+
+        [HttpPost]
+        public ActionResult<Turnos> Post([FromBody] Turnos turno)
+        {
+            _context.ControlTurnos.Add(turno);
+            _context.SaveChanges();
+            return new CreatedAtRouteResult("ObtenerTurnoporid", new { id = turno.Id_Turno }, turno);
+        }
+        [HttpPut("{id}")]
+        public ActionResult<Turnos> Put(int id, [FromBody] Turnos turno)
+        {
+            if (id != turno.Id_Turno)
+            {
+                return BadRequest();
+            }
+            _context.Entry(turno).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult<Turnos> Delete(int id)
+        {
+            try
+            {
+                var turno = _context.ControlTurnos.FirstOrDefault(p => p.Id_Turno == id);
+                if (turno == null)
+                {
+                    return NotFound();
+                }
+                _context.ControlTurnos.Remove(turno);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+                throw;
+            }
+        }
     }
 }
