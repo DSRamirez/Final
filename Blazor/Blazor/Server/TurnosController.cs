bd3f6e7c89a94137a311a96afa1aa3b9a77181a3
Ezequiel Trejo Mercado <ezetrejom75@gmail.com>
2021-03-26 18:24:30 -0300

    .

---
+++ Blazor/Blazor/Server/TurnosController.cs
@@ -0,0 +1,30 @@
+﻿using Blazor.Server.Data;
+using Blazor.Server.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Blazor.Server
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TurnosController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public TurnosController(ApiContext context)
+        {
+            _context = context;
+        }
+
+        //Get: api/Turnos
+        [HttpGet]
+        public ActionResult<IEnumerable<Turnos>> GetTurnosItems()
+        {
+            return _context.ControlTurnos.ToList();
+        }
+
+
+    }
+}
